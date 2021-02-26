121529ea1fdc5be8c9c111a48bc19f782030010d
Fei Lu <dr.lufei@gmail.com>
2021-02-26 21:30:28 +0800

    fastcall2 binsize = 5000000

--- src/pgl/app/fastCall2/FastCall2.java
+++ src/pgl/app/fastCall2/FastCall2.java
@@ -12,7 +12,7 @@ public class FastCall2 {
     //Current step ID of the pipeline
     int step = Integer.MIN_VALUE;
     //genome block for individual ing
-    static int binSize = 50000;
+    static int binSize = 500000;
     //A, C, G, T, -, +
     static final byte[] pileupAlleleAscIIs = {65, 67, 71, 84, 45, 43};
 
