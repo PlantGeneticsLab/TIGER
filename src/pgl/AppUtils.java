6a2b3c1176d71dc974081ee0ed338b48c9942da0
Fei Lu <dr.lufei@gmail.com>
2021-07-04 21:56:25 +0800

    AppUtils add function to generate parameter files

--- src/pgl/AppUtils.java
+++ src/pgl/AppUtils.java
@@ -4,6 +4,7 @@ import pgl.infra.utils.Dyad;
 import pgl.infra.utils.IOUtils;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +14,11 @@ import java.util.List;
  * @author feilu
  */
 public class AppUtils {
-
+    /**
+     * Return the list of steps and the list of parameters
+     * @param parameterFileS
+     * @return
+     */
     public static Dyad<List<String>, List<String>> getParameterList (String parameterFileS) {
         List<String> pLineList = new ArrayList<>();
         List<String> sLineList = new ArrayList<>();
@@ -36,5 +41,42 @@ public class AppUtils {
             System.exit(1);
         }
         return d;
+    }
+
+    public static void creatParameterFile (String sampleParameterFileS, List<String> sLineList, List<String> pLineList, String outfileS) {
+        try {
+            int sCnt = 0;
+            int pCnt = 0;
+            BufferedReader br = IOUtils.getTextReader(sampleParameterFileS);
+            BufferedWriter bw = IOUtils.getTextWriter(outfileS);
+            String temp = null;
+            while ((temp = br.readLine()) != null) {
+                if (temp.startsWith("#")) {
+                    bw.write(temp);
+                }
+                else if (temp.startsWith("@")) {
+                    bw.write(sLineList.get(sCnt));
+                    sCnt++;
+                }
+                else if (temp.length() == 0) {
+
+                }
+                else if (temp.startsWith("\\s+")) {
+                    bw.write(temp);
+                }
+                else {
+                    bw.write(pLineList.get(pCnt));
+                    pCnt++;
+                }
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
